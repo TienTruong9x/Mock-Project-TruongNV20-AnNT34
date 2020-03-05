f741c21c95e381388f1f8cbbbade4a0528737c11
TienTruong <nguyenthientruong1998@gmail.com>
2020-03-05 14:56:01 +0700

    Moi them component sign in

--- MockProject/src/app/app.module.ts
+++ MockProject/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
