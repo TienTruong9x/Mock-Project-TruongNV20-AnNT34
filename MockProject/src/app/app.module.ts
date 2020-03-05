9fe1131504813ec9e037ad2aefbd037e4e09c564
unknown <jacksperrow12699@gmail.com>
2020-03-05 14:55:47 +0700

    Mới thêm cái home

--- MockProject/src/app/app.module.ts
+++ MockProject/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
