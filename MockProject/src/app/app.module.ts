291c821f49ac3cd74e9772325fbd9235b619708e
TienTruong <nguyenthientruong1998@gmail.com>
2020-03-05 15:05:32 +0700

    Ghep thanh cong 2 component

--- MockProject/src/app/app.module.ts
+++ MockProject/src/app/app.module.ts
@@ -4,21 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-<<<<<<< HEAD
 import { SignInComponent } from './sign-in/sign-in.component';
-=======
 import { HomeComponent } from './home/home.component';
->>>>>>> 9fe1131504813ec9e037ad2aefbd037e4e09c564
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-<<<<<<< HEAD
-    SignInComponent
-=======
+    SignInComponent,
     HomeComponent
->>>>>>> 9fe1131504813ec9e037ad2aefbd037e4e09c564
   ],
   imports: [
     BrowserModule,
