cb4e33d639eb088f8ff61076a74f05798628f75b
TienTruong9x <59884685+TienTruong9x@users.noreply.github.com>
2020-03-06 05:22:21 +0700

    Xóa các file rác,Tạo cấu trúc folder App

--- MockProject/src/app/app.module.ts
+++ MockProject/src/app/app.module.ts
@@ -2,17 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+//Component
 import { AppComponent } from './app.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SignUpComponent,
-    SignInComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
