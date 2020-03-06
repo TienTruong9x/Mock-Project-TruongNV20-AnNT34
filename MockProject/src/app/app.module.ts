30aa37a5bf04f50bccc3660a1e359f227f81460c
TienTruong9x <59884685+TienTruong9x@users.noreply.github.com>
2020-03-06 07:04:59 +0700

    Thêm các component, vẽ sơ lược cấu trúc

--- MockProject/src/app/app.module.ts
+++ MockProject/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-//Component
+
+//Component Import
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './main-header/header/header.component';
+import { SignInComponent } from './main-body/authentication/sign-in/sign-in.component';
+import { SignUpComponent } from './main-body/authentication/sign-up/sign-up.component';
+import { FooterComponent } from './main-footer/footer/footer.component';
+import { FeedPageComponent } from './main-body/news-feed/feed-page/feed-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //main-header
+    HeaderComponent,
+    SignInComponent,
+    SignUpComponent,
+    FooterComponent,
+    FeedPageComponent
+    //main-body
+    //main-footer
   ],
   imports: [
     BrowserModule,
